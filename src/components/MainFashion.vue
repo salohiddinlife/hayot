e471201b92c61813f3a8975eb83420138fc90c3e
salohiddinlife <z1565400@gmail.com>
2024-01-17 11:57:45 +0000

    second commit

---
+++ src/components/MainFashion.vue
@@ -0,0 +1,15 @@
+<template>
+    <section class="main__fashion">
+        <div class="container">
+            
+        </div>
+    </section>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
