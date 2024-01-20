c78a133775dd253abce335b41acf742a34e213a1
salohiddinlife <z1565400@gmail.com>
2024-01-20 13:50:05 +0000

    seventh commmit

--- src/components/MainReviews.vue
+++ src/components/MainReviews.vue
@@ -7,9 +7,63 @@
     <div class="main__reviews-items">
       <div class="main__reviews-item">
         <div class="main__reviews-item-imgs">
-          <img src="" alt="" class="main__reviews-item-img">
-          <span class="rating"></span>
+          <img
+            src="/src/assets/img/review1.svg"
+            alt=""
+            class="main__reviews-item-img"
+          />
+          <div class="rating">
+            <input value="5" name="rate" id="star5" type="radio" />
+            <label title="text" for="star5"></label>
+            <input value="4" name="rate" id="star4" type="radio" />
+            <label title="text" for="star4"></label>
+            <input value="3" name="rate" id="star3" type="radio" checked="" />
+            <label title="text" for="star3"></label>
+            <input value="2" name="rate" id="star2" type="radio" />
+            <label title="text" for="star2"></label>
+            <input value="1" name="rate" id="star1" type="radio" />
+            <label title="text" for="star1"></label>
+          </div>
         </div>
+        <h3 class="main__reviews-item-title">Floyd Miles</h3>
+        <p class="main__reviews-item-text">
+          Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+          sint. Velit officia consequat duis enim velit mollit. Exercitation
+          veniam consequat sunt nostrud amet. Amet minim mollit non deserunt
+          ullamco est sit aliqua dolor do amet sint. Velit officia consequat
+          duis enim velit mollit. Exercitation veniam consequat sunt nostrud
+          amet.
+        </p>
+      </div>
+      <div class="main__reviews-item">
+        <div class="main__reviews-item-imgs">
+          <img
+            src="/src/assets/img/review1.svg"
+            alt=""
+            class="main__reviews-item-img"
+          />
+          <div class="rating">
+            <input value="5" name="rate" id="star5" type="radio" />
+            <label title="text" for="star5"></label>
+            <input value="4" name="rate" id="star4" type="radio" />
+            <label title="text" for="star4"></label>
+            <input value="3" name="rate" id="star3" type="radio" checked="" />
+            <label title="text" for="star3"></label>
+            <input value="2" name="rate" id="star2" type="radio" />
+            <label title="text" for="star2"></label>
+            <input value="1" name="rate" id="star1" type="radio" />
+            <label title="text" for="star1"></label>
+          </div>
+        </div>
+        <h3 class="main__reviews-item-title">Floyd Miles</h3>
+        <p class="main__reviews-item-text">
+          Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+          sint. Velit officia consequat duis enim velit mollit. Exercitation
+          veniam consequat sunt nostrud amet. Amet minim mollit non deserunt
+          ullamco est sit aliqua dolor do amet sint. Velit officia consequat
+          duis enim velit mollit. Exercitation veniam consequat sunt nostrud
+          amet.
+        </p>
       </div>
     </div>
   </section>
