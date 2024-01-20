4e440bc838516e84e517bd6b3f6b8e14966cba60
salohiddinlife <z1565400@gmail.com>
2024-01-20 12:11:07 +0000

    fifth commit

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -5,7 +5,35 @@
     <Arrivals/>
     <Saving/>
     <Fashion/>
+    <ForMens/><template>
+<main class="main">
+    <Slider/>
+    <SaleProducts/>
+    <Arrivals/>
+    <Saving/>
+    <Fashion/>
     <ForMens/>
+    <ForWomens/>
+    <Reviews/>
+</main>
+</template>
+
+<script setup>
+import Slider from '@/components/MainSlider.vue'
+import SaleProducts from '@/components/MainSaleProducts.vue'
+import Arrivals from '@/components/MainArrivals.vue'
+import Saving from '@/components/MainSaving.vue'
+import Fashion from '@/components/MainFashion.vue'
+import ForMens from '@/components/MainForMens.vue'
+import ForWomens from '@/components/MainForWomens.vue'
+import Limelight from '@/components/MainLimeLight.vue'
+import Reviews from '@/components/MainReviews.vue'
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
     <ForWomens/>
 </main>
 </template>
--- src/components/MainReviews.vue
+++ src/components/MainReviews.vue
@@ -1,13 +1,20 @@
 <template>
-  <div>
-
-  </div>
+  <section class="main__reviews">
+    <div class="title">
+      <span class="span"></span>
+      <p class="text">Reviews</p>
+    </div>
+    <div class="main__reviews-items">
+      <div class="main__reviews-item">
+        <div class="main__reviews-item-imgs">
+          <img src="" alt="" class="main__reviews-item-img">
+          <span class="rating"></span>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
-<script setup>
+<script setup></script>
 
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
